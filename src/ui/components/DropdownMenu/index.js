e9c9c4ece1cf6d87fffdf1c34841b571531061b1
Matthew Riley MacPherson (tofumatt) <hi@tofumatt.com>
2017-10-25 15:42:00 +0100

    fix: Close dropdown on click (fix #3452)

--- src/ui/components/DropdownMenu/index.js
+++ src/ui/components/DropdownMenu/index.js
@@ -1,8 +1,10 @@
 /* @flow */
+import { oneLine } from 'common-tags';
 import * as React from 'react';
 import classNames from 'classnames';
 import onClickOutside from 'react-onclickoutside';
 
+import log from 'core/logger';
 import Icon from 'ui/components/Icon';
 import DropdownMenuItem from 'ui/components/DropdownMenuItem';
 
@@ -34,18 +36,44 @@ export class DropdownMenuBase extends React.Component<Props, State> {
     }));
   }
 
+  handleOnClickForLinks = (event: SyntheticEvent<any>) => {
+    // If a link inside the menu is clicked, we should close the dropdown.
+    // See: https://github.com/mozilla/addons-frontend/issues/3452
+    if (event.target && event.target.tagName === 'A') {
+      log.debug(oneLine`Setting state of DropdownMenu to buttonIsActive to
+        false, because a link inside the menu was clicked.`);
+      this.setState({ buttonIsActive: false });
+    }
+  }
+
   handleClickOutside = () => {
+    this.setState({ buttonIsActive: false });
+  }
+
+  handleOnMouseEnter = () => {
+    this.setState({ buttonIsActive: true });
+  }
+
+  handleOnMouseLeave = () => {
     this.setState({ buttonIsActive: false });
   }
 
   render() {
     const { children, className, text } = this.props;
 
+    // ESLint doesn't like the event handlers we attach to the
+    // div below, but they're just re-creating hover in JS and dismissing
+    // the menu when links inside it are clicked, so it's not really
+    // an interactive element.
+    /* eslint-disable jsx-a11y/no-static-element-interactions */
     return (
       <div
         className={classNames('DropdownMenu', className, {
           'DropdownMenu--active': this.state.buttonIsActive,
         })}
+        onClick={this.handleOnClickForLinks}
+        onMouseEnter={this.handleOnMouseEnter}
+        onMouseLeave={this.handleOnMouseLeave}
       >
         <button
           className="DropdownMenu-button"
@@ -65,6 +93,7 @@ export class DropdownMenuBase extends React.Component<Props, State> {
         )}
       </div>
     );
+    /* eslint-enable jsx-a11y/no-static-element-interactions */
   }
 }
 
